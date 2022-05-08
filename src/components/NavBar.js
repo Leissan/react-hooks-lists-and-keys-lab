b16f3016a89a7040e12ccb5216c7685ef43da610
Leissan Khan <leissan.khan@gmail.com>
2022-05-08 15:44:35 -0400

    finish laB

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {nanoid} from 'nanoid'
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>
+    <a id={nanoid()} href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#projects">Projects</a>
+  </nav>
+  );
 }
 
 export default NavBar;
